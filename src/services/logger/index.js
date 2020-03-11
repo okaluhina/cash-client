f815625822fc519339323f07746c909cbcced3cf
Volha.Kaluhina <Volha.Kaluhina@itechart-group.com>
2020-03-11 12:02:02 +0300

    feat: tgfhcvjh

---
+++ src/services/logger/index.js
@@ -0,0 +1,25 @@
+import log4js from 'log4js';
+
+log4js.configure({
+  appenders: {
+    file: {
+      type: 'file',
+      filename: 'logs/client.log',
+      maxLogSize: 10 * 1024 * 1024,
+      backups: 25,
+      compress: true,
+      encoding: 'utf-8',
+      mode: 0o0640
+    },
+    out: {
+      type: 'stdout'
+    }
+  },
+  categories: {
+    default: { appenders: ['file', 'out'], level: 'trace' }
+  }
+});
+
+const logger = log4js.getLogger('api');
+
+export default logger;
--- src/sw-build.js
+++ src/sw-build.js
@@ -1,26 +1,32 @@
-import workboxBuild from "workbox-build";
+const {injectManifest, generateSW} = require("workbox-build");
+
+// const logger = require('./services/logger');
+
 
 const buildSW = () => {
+
+  console.log('==============START TO BUILD SERVICE WORKER1================')
   // The build is expected to fail if the 
   // sw install rules couldn't be generated.
   // Add a catch block to handle this scenario.
-  ​return workboxBuild.injectManifest({
 
-   ​swSrc: "src/sw-custom.js", // custom sw rule
-
-   ​swDest: "build/sw.js", // sw output file (auto-generated
-
-   ​globDirectory: "build",
-
-   ​globPatterns: ["**/*.{js,css,html,png,svg}"],
-
-   ​maximumFileSizeToCacheInBytes: 5 * 1024 * 102
+  console.log(injectManifest);
+  return injectManifest({
+      "​swSrc": "src/sw-custom.js",
+      "swDest": "build/sw.js",
+      "​globDirectory": "build",
+      "​globPatterns": ["**/*.{js,css,html,png,svg}"],
+      "​maximumFileSizeToCacheInBytes": 5 * 1024 * 102
   })
-  .then(({ count, size, warnings }) => {
-    warnings.forEach(console.warn);
-    console.info(`${count} files will be precached, 
-                  totaling ${size/(1024 * 1024)} MBs.`);
-  });
+  // .then(({ count, size, warnings }) => {
+  //   // warnings.forEach(console.warn);
+  //   console.info(`${count} files will be precached, 
+  //                 totaling ${size/(1024 * 1024)} MBs.`);
+  // }).catch((err) => { console.log(err)});
 };
 
-buildSW();
+ console.log('START TO BUILD SERVICE WORKER2');
+
+buildSW()
+// "build": "react-scripts build && npm run build-sw&& npm run clean-cra-sw",
+// 
